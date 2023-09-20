be0ab5a0197eb99424c7cd1a53484081976eed80
ErikJava <112395602+ErikJava@users.noreply.github.com>
2023-09-20 23:41:22 +0200

    Third update

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:flutter_app/pages/contact.dart';
 import 'package:flutter_app/pages/home.dart';
 
 void main() => runApp(MaterialApp(
+  debugShowCheckedModeBanner: false,
   initialRoute: '/home',
   routes: {
     '/home': (context) => const Home(),
